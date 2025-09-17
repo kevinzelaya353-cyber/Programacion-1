9055ede6478ea037bc98358f2db7982a45ecd549
Kevin Zelaya <kevinzelaya353@gmail.com>
2025-09-17 17:19:24 -0600

    cambios varios

--- src/proyecto/progra/pkg1/grupo/pkg2/Empleado.java
+++ src/proyecto/progra/pkg1/grupo/pkg2/Empleado.java
@@ -1,67 +1,100 @@
-package proyecto.progra.pkg1.grupo.pkg2;
-
-
-import java.util.Scanner;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package proyecto.progra.pkg1.grupo.pkg2;
 
 /**
  *
  * @author ingen
  */
 public class Empleado {
-    Scanner sc= new Scanner (System.in);
+     public int id;
+    public String nombre;
+    public String apellido;
+    public double salario;
 
-    private double salario;
-    private int id;
-    private String nombre;
-    private String apellido;
-    private double pagohora; // el pago que se le da por hora extra 
-    private double horastrab; //las horas extra que trabaja durante el dia
+    public double pagohora;   // pago por hora extra
+    public double horastrab;  // horas extra trabajadas
+    public double diasTrabajados;
 
-    public Empleado(String nombre, String apellido, double salario) {
-
+    public Empleado(int id, String nombre, String apellido, double salario) {
+        this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.salario = salario;
-        this.id = id;
         this.pagohora = 0;
         this.horastrab = 0;
-    }
-    
-    public String getnombre(){
-        return this.nombre;
-    }
-    public void setnombre(String nombre){
-        this.nombre= nombre;
-    }
-    public String getapellido(){
-        return this.apellido;
-    }
-    public void setapellido(String apellido){
-        this.apellido= apellido;
+        this.diasTrabajados = 0;
     }
 
-    public double getsalario() {
-        return this.salario;
+    public int getId() {
+        return id;
     }
 
-    public void setsalario(double salario) {
-        this.salario = salario;   
-    }
-    
-   
-
-    public void mostrardatos() {
-        System.out.println("------------------");
-        System.out.printf("Nombre del Empleado: %s\n", this.nombre);
-        System.out.printf("Apellido del Empleado: %s\n", this.apellido);
-        System.out.printf("Salario del Empleado: %.2f\n", this.salario);
-        System.out.printf("ID Empleado: %d\n", this.id);
-
+    public void setId(int idNuevo) {
+        this.id = idNuevo;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombreNuevo) {
+        this.nombre = nombreNuevo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellidoNuevo) {
+        this.apellido = apellidoNuevo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double nuevoSalario) {
+        this.salario = nuevoSalario;
+    }
+
+    public double getHorastrab() {
+        return horastrab;
+    }
+
+    public void setHorastrab(double horastrab) {
+        this.horastrab = horastrab;
+    }
+
+    public double getPagohora() {
+        return pagohora;
+    }
+
+    public void setPagohora(double pagohora) {
+        this.pagohora = pagohora;
+    }
+
+    public double getDiasTrabajados() {
+        return diasTrabajados;
+    }
+
+    public void setDiasTrabajados(double diasTrabajados) {
+        this.diasTrabajados = diasTrabajados;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("ID: " + id);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Salario por dia: Lps." + salario);
+        System.out.println("Dias trabajados: " + diasTrabajados);
+        System.out.println("Horas extra: " + horastrab);
+        System.out.println("Pago por hora extra: Lps." + pagohora);
+    }
+
+    public String toString() {
+        return nombre + " | " + apellido + " | Lps." + salario + " | ID: 00" + id;
+    }
 }
---
+++ src/proyecto/progra/pkg1/grupo/pkg2/ProyectoProgra1VersionFinalGrupo2.java
@@ -0,0 +1,237 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.progra.pkg1.grupo.pkg2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ingen
+ */
+public class ProyectoProgra1VersionFinalGrupo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Empleado> empleados = new ArrayList<>();
+
+        int opcion = 0;
+        int id = 4;
+
+        // Guarde 3 empleados de muestra para no tener que agregar tanto manualmente y asi poder probar el codigo
+        Empleado emp1 = new Empleado(1, "Kevin", "Zelaya", 200);
+        empleados.add(emp1);
+        Empleado emp2 = new Empleado(2, "Luis", "Barahona", 100);
+        empleados.add(emp2);
+        Empleado emp3 = new Empleado(3, "Josue", "Medina", 150);
+        empleados.add(emp3);
+
+        do {
+            System.out.println("\n---------------------------------------------");
+            System.out.println("1. Agregar nuevo colaborador");
+            System.out.println("2. Ver todos los colaboradores");
+            System.out.println("3. Agregar dias trabajados");
+            System.out.println("4. Agregar horas extra y valor por hora");
+            System.out.println("5. Ver resumen sin deducciones");
+            System.out.println("6. Ver total mensual (salario + horas extra)");
+            System.out.println("7. Ver reporte con deducciones");
+            System.out.println("8. Eliminar colaborador");
+            System.out.println("9. Salir");
+            System.out.println("---------------------------------------------\n");
+            System.out.print("Seleccione una opcion: ");
+            opcion = sc.nextInt();
+            System.out.println("\n---------------------------------------------\n");
+
+            switch (opcion) {
+
+                case 1:
+                    int agregar = 1;
+                    while (agregar == 1) {
+                        System.out.println("Ingrese el nombre del colaborador:");
+                        String nombre = sc.next();
+
+                        System.out.println("Ingrese el apellido del colaborador:");
+                        String apellido = sc.next();
+
+                        System.out.println("Ingrese el salario por dia:");
+                        double salario = sc.nextDouble();
+
+                        Empleado nuevoEmpleado = new Empleado(id, nombre, apellido, salario);
+                        empleados.add(nuevoEmpleado);
+
+                        id = id + 1;
+
+                        System.out.println("Desea agregar otro colaborador? (1 = si / 2 = no):");
+                        agregar = sc.nextInt();
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("\nLista de colaboradores:");
+                    for (int i = 0; i < empleados.size(); i++) {
+                        Empleado e = empleados.get(i);
+                        System.out.println(e.getNombre() + " | " + e.getApellido() + " | Lps." + e.getSalario() + " | ID: 00" + e.getId());
+                    }
+                    break;
+
+                case 3:
+
+                    int idBuscado = 0;
+
+                    System.out.println("Ingrese el ID del colaborador:");
+                    idBuscado = sc.nextInt();
+                    Empleado encontrado = null;
+
+                    for (int i = 0; i < empleados.size(); i++) {
+                        Empleado e = empleados.get(i);
+                        if (e.getId() == idBuscado) {
+                            encontrado = e;
+                            break;
+                        }
+                    }
+
+                    if (encontrado != null) {
+                        System.out.println("Colaborador encontrado: " + encontrado.getNombre() + " " + encontrado.getApellido());
+                        System.out.println("Cuantos dias trabajo?");
+                        double dias = sc.nextDouble();
+
+                        encontrado.setDiasTrabajados(dias);
+
+                        double total = dias * encontrado.getSalario();
+                        System.out.println("Salario total sin extras: Lps." + total);
+                    } else {
+                        System.out.println("No se encontro ningun colaborador con ese ID.");
+                    }
+                    break;
+
+                case 4:
+
+                    int idExtra = 0;
+
+                    System.out.println("Ingrese el ID del colaborador:");
+                    idExtra = sc.nextInt();
+                    Empleado encontradoExtra = null;
+
+                    for (int i = 0; i < empleados.size(); i++) {
+                        if (empleados.get(i).getId() == idExtra) {
+                            encontradoExtra = empleados.get(i);
+                            break;
+                        }
+                    }
+
+                    if (encontradoExtra != null) {
+                        System.out.println("Colaborador encontrado: " + encontradoExtra.getNombre() + " " + encontradoExtra.getApellido());
+                        System.out.println("Ingrese cantidad de horas extra:");
+                        double horas = sc.nextDouble();
+                        System.out.println("Ingrese el pago por hora extra:");
+                        double valor = sc.nextDouble();
+
+                        encontradoExtra.setHorastrab(horas);
+                        encontradoExtra.setPagohora(valor);
+                        System.out.println("Datos de horas extra guardados.");
+                    } else {
+                        System.out.println("ID no encontrado.");
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("\nTOTAL mensual incluyendo horas extra:");
+                    for (int i = 0; i < empleados.size(); i++) {
+                        Empleado e = empleados.get(i);
+                        double dias = e.getDiasTrabajados();
+                        double extras = e.getHorastrab() * e.getPagohora();
+                        double base = e.getSalario() * e.getDiasTrabajados();
+                        double total = base + extras;
+                        System.out.println("ID: " + e.getId());
+                        System.out.println("Nombre: " + e.getNombre() + " " + e.getApellido());
+                        System.out.println("Dias: " + dias);
+                        System.out.println("Salario base: Lps." + base);
+                        System.out.println("Horas extra: Lps." + extras);
+                        System.out.println("TOTAL MES: Lps." + total);
+                        System.out.println("-----------------------------");
+                    }
+                    break;
+
+                case 6:
+                    System.out.println("\nResumen sin deducciones:");
+                    for (int i = 0; i < empleados.size(); i++) {
+                        Empleado e = empleados.get(i);
+                        System.out.println("ID: " + e.getId());
+                        System.out.println("Nombre: " + e.getNombre());
+                        System.out.println("Apellido: " + e.getApellido());
+                        System.out.println("Salario por dia: Lps." + e.getSalario());
+                        System.out.println("Dias trabajados: " + e.getDiasTrabajados());
+                        double salarioBase = e.getSalario() * e.getDiasTrabajados();
+                        System.out.println("Salario total sin extras: Lps." + salarioBase);
+                        System.out.println("Horas extra: " + e.getHorastrab());
+                        System.out.println("Pago por hora extra: Lps." + e.getPagohora());
+                        System.out.println("-----------------------------");
+                    }
+                    break;
+
+                case 7:
+                    System.out.println("\nReporte mensual con deducciones (10%):");
+                    for (int i = 0; i < empleados.size(); i++) {
+                        Empleado e = empleados.get(i);
+                        double pagoExtra = e.getHorastrab() * e.getPagohora();
+                        double base = e.getSalario() * e.getDiasTrabajados();
+                        double total = base + pagoExtra;
+                        double descuento = total * 0.10;
+                        double finalTotal = total - descuento;
+                        System.out.println("Colaborador: " + e.getNombre() + " " + e.getApellido());
+                        System.out.println("Salario base: Lps." + base);
+                        System.out.println("Pago horas extra: Lps." + pagoExtra);
+                        System.out.println("Deduccion (10%): Lps." + descuento);
+                        System.out.println("TOTAL FINAL: Lps." + finalTotal);
+                        System.out.println("-----------------------------");
+                    }
+                    break;
+
+                case 8:
+
+                    int idEliminar = 0;
+
+                    System.out.println("Ingrese el ID del colaborador a eliminar:");
+                    idEliminar = sc.nextInt();
+
+                    Empleado eliminar = null;
+
+                    for (int i = 0; i < empleados.size(); i++) {
+                        if (empleados.get(i).getId() == idEliminar) {
+                            eliminar = empleados.get(i);
+                            break;
+                        }
+                    }
+
+                    if (eliminar != null) {
+                        System.out.println("Seguro que desea eliminar a " + eliminar.getNombre() + "? (1 = si / 2 = no):");
+                        int confirm = sc.nextInt();
+                        if (confirm == 1) {
+                            empleados.remove(eliminar);
+                            System.out.println("Colaborador eliminado.");
+                        } else {
+                            System.out.println("Eliminacion cancelada.");
+                        }
+                    } else {
+                        System.out.println("No se encontro un colaborador con ese ID.");
+                    }
+                    break;
+
+                case 9:
+                    System.out.println("Cerrando sesion...");
+                    break;
+
+                default:
+                    System.out.println("Opcion invalida.");
+                    break;
+            }
+
+        } while (opcion != 9);
+
+    } // fin de main 
+}//fin de class
